01050549bfa3d1d18640ba7df4062ea4a3ef7457
shayan jafari <shayanja12345@gmail.com>
2022-09-10 19:11:02 +0430

    create pages website

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ function App() {
       <div className="flex flex-col justify-betwin h-screen">
         <Navbar />
         <main className="container mx-auto px-3 pb-12">
-          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
         </main>
         <Footer />
       </div>
